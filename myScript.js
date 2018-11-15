8317946b7aa4843a4aa8df00f613c24d545fb761
Randy Pham <randy.default@gmail.com>
2018-11-14 19:07:06 -0700

    Update 11/14/18 - Changed background, added footer
    changed background so webpages are more readable. Added a sweet footer too.

--- myScript.js
+++ myScript.js
@@ -3,11 +3,11 @@ function recipeClick() {
   var i;
 
   for (i = 0; i < button.length; i++) {
-    button[i].addEventListener("click", function() {
+    button[i].addEventListener("click", function () {
       this.classList.toggle("active");
       var recipe = this.nextElementSibling;
 
-      if (recipe.style.maxHeight){
+      if (recipe.style.maxHeight) {
         recipe.style.maxHeight = null;
       } else {
         recipe.style.maxHeight = recipe.scrollHeight + "px";
